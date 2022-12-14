dbbbafece5139db950b04a041994dbdb36c71a40
Aditya Shukla <100330745+aditya7ss@users.noreply.github.com>
2022-12-14 23:17:05 +0530

    Initial commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,42 @@
+
+
+
+import wallpaper1 from "./Wallpaper0.jpg";
+import wallpaper2 from "./wallpaper2.jpg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import coverflow from "./coverflow.png";
+
+import games from "./games.jpeg";
+import allsongs from "./allsongs.gif";
+import artists from "./artists.jpg";
+import albums from "./albums.png";
+ import song0img from "./Excuses.jpg";
+import songImg from "./Apsraa.jpg"
+
+import song1Img from "./stay.png";
+import song2Img from "./deserve-you.jpeg";
+import song3Img from "./yummy.png";
+import song4Img from "./dekh-lena.jpg"
+
+const images = {
+	
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song0img,songImg,
+	song1Img,
+	song2Img,
+	song3Img,
+	song4Img
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,16 @@
+import music0 from "./Excuses-AP_Dhillon.mp3"
+import music from "./Apsraa.mp3"
+import music1 from "./stay.mp3";
+import music2 from "./deserve-you.mp3";
+import music3 from "./yummy.mp3";
+import music4 from "./Tum_Bin.mp3"
+
+const songs = {
+	music0,music,
+	music1,
+	music2,
+	music3,
+	music4,
+};
+
+export default songs;
